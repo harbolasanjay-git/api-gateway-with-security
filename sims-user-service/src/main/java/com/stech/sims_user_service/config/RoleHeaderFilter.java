f111c44f823270613db29bfd91f0538af1677f62
harbolasanjay-git <sanjayharbola.study@gmail.com>
2025-03-29 23:47:09 +0530

    change folder name from cnfig to config

--- sims-user-service/src/main/java/com/stech/sims_user_service/config/RoleHeaderFilter.java
+++ sims-user-service/src/main/java/com/stech/sims_user_service/config/RoleHeaderFilter.java
@@ -1,15 +1,13 @@
-package com.stech.sims_user_service.cnfig;
+package com.stech.sims_user_service.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
--- sims-user-service/src/main/java/com/stech/sims_user_service/config/SecurityConfig.java
+++ sims-user-service/src/main/java/com/stech/sims_user_service/config/SecurityConfig.java
@@ -1,24 +1,14 @@
-package com.stech.sims_user_service.cnfig;
+package com.stech.sims_user_service.config;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.vote.AffirmativeBased;
-import org.springframework.security.access.vote.AuthenticatedVoter;
-import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
-
-import java.util.List;
 
 @EnableMethodSecurity
 @Configuration
